2c99e5cf2bcb22a7fdf05282cea20144fe63a374
EECOLOR <eecolor@qirx.org>
2019-05-23 22:56:04 +0200

    updated comment

--- webpack/config/plugin-config.js
+++ webpack/config/plugin-config.js
@@ -1,1 +1,4 @@
 // TODO: help compile plugins with the correct format
+//
+// This should be a webpack config that is specialized for plugins, we need this config for ourselves
+// so why not expose it to the public and make it easier for them to compile their plugin?
